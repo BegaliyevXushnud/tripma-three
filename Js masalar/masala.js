0b24aae4b79411ec1d6b5b034015735bbdbc5e97
BegaliyevXushnud <xushnudpubgmn1@gmail.com>
2024-06-28 12:53:25 +0500

    added about

--- Js masalar/masala.js
+++ Js masalar/masala.js
@@ -1,6 +1,77 @@
-let a = +prompt("Son kiriting");
-let b = +prompt(" Ikkinchi Sonni kiriting");
-let c = +prompt("Uchinchi sonni kiriting");
-if(a > b){
-    
-}
+// let num1 = 10;
+// let num2 = 25;
+// let num3 = 15;
+// let natija;
+
+// if (num1 >= num2) {
+//     if (num1 >= num3) {
+//         largest = num1;
+//     } else {
+//         largest = num3;
+//     }
+// } else {
+//     if (num2 >= num3) {
+//         largest = num2;
+//     } else {
+//         largest = num3;
+//     }
+// }
+
+// console.log("Eng katta son: " + natija);
+
+// 1-masala
+
+
+
+
+// let mablag = prompt("Mablag'ingizni kiriting:");
+// let xonaTuri;
+
+// if (mablag >= 1000) {
+//     xonaTuri = "Lux xona";
+// } else if (mablag >= 500) {
+//     xonaTuri = "Yarim lux xona";
+// } else if (mablag >= 200) {
+//     xonaTuri = "Oddiy xona";
+// } else {
+//     xonaTuri = "Joy yo'q";
+// }
+
+// console.log("Siz uchun mavjud xona turi: " + xonaTuri);
+
+//2-masala
+
+
+
+
+// let son = prompt("Son kiriting:");
+// son = parseInt(son, 10);
+
+// if (son % 3 === 0 && son % 5 === 0) {
+//     console.log("fizz-bazz");
+// } else if (son % 3 === 0) {
+//     console.log("fizz");
+// } else if (son % 5 === 0) {
+//     console.log("bass");
+// } else {
+//     console.log("Son 3 ga ham, 5 ga ham bo'linmaydi");
+// }
+// 3-masala 
+
+
+// let num1 = prompt("Birinchi sonni kiriting:");
+// let num2 = prompt("Ikkinchi sonni kiriting:");
+// let num3 = prompt("Uchinchi sonni kiriting:");
+
+// let engKichik;
+
+// if (num1 <= num2 && num1 <= num3) {
+//     engKichik = num1;
+// } else if (num2 <= num1 && num2 <= num3) {
+//     engKichik = num2;
+// } else {
+//     engKichik = num3;
+// }
+
+// console.log("Eng kichik son: " + engKichik);
+// 4-masala
